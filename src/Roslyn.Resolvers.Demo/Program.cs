955232efddaf8bf81d522bbd47068952a6f276fc
filipw <wojcieszyn@gmail.com>
2016-06-14 21:10:04 +0200

    demo commit

---
+++ src/Roslyn.Resolvers.Demo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Immutable;
+
+namespace Roslyn.Resolvers.Demo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var code = @"
+                #load ""https://gist.githubusercontent.com/filipw/9a79bb00e4905dfb1f48757a3ff12314/raw/adbfe5fade49c1b35e871c49491e17e6675dd43c/foo.csx""
+                #load ""foo.csx""
+                Console.WriteLine(""Hello"");
+            ";
+
+            var opts = ScriptOptions.Default.
+                AddImports("System").
+                WithSourceResolver(new RemoteFileResolver(ImmutableArray.Create(new string[0]), 
+                AppContext.BaseDirectory));
+
+            var script = CSharpScript.Create(code, opts);
+            var result = script.RunAsync().Result;
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Roslyn.Resolvers.Demo/RemoteFileResolver.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Net.Http;
+
+namespace Roslyn.Resolvers.Demo
+{
+    public class RemoteFileResolver : SourceReferenceResolver
+    {
+        private Dictionary<string, Stream> remoteFiles = new Dictionary<string, Stream>();
+        private SourceFileResolver fileBasedResolver;
+
+        public RemoteFileResolver(ImmutableArray<string> searchPaths, string baseDirectory)
+        {
+            fileBasedResolver = new SourceFileResolver(searchPaths, baseDirectory);
+        }
+
+        public override bool Equals(object other)
+        {
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return -1;
+        }
+
+        public override string NormalizePath(string path, string baseFilePath)
+        {
+            var uri = GetUri(path);
+            if (uri == null) return fileBasedResolver.NormalizePath(path, baseFilePath);
+
+            return path;
+        }
+
+        public override Stream OpenRead(string resolvedPath)
+        {
+            var uri = GetUri(resolvedPath);
+            if (uri == null) return fileBasedResolver.OpenRead(resolvedPath);
+
+            return remoteFiles[resolvedPath];
+        }
+
+        public override string ResolveReference(string path, string baseFilePath)
+        {
+            var uri = GetUri(path);
+            if (uri == null) return fileBasedResolver.ResolveReference(path, baseFilePath);
+
+            var client = new HttpClient();
+            var response = client.GetAsync(path).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                remoteFiles.Add(path, response.Content.ReadAsStreamAsync().Result);
+            }
+            return path;
+        }
+
+        private Uri GetUri(string input)
+        {
+            Uri uriResult;
+            if (Uri.TryCreate(input, UriKind.Absolute, out uriResult)
+                          && (uriResult.Scheme == "http"
+                              || uriResult.Scheme == "https"))
+            {
+                return uriResult;
+            }
+
+            return null;
+        }
+    }
+}
